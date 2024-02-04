aaad49f3d9467b61dc451ba2ab8f7a95c306730b
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-04 00:40:22 -0500

    scripted commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -l
 
-echo "$1" > temp.txt
-echo "$2" >> temp.txt
-echo "$3" >> temp.txt
-echo "$4" >> temp.txt
-echo "$5" >> temp.txt
-echo "$6" >> temp.txt
-echo "$7" >> temp.txt
+echo "$1" > docker-repo.txt
+echo "$2" >> username.txt
+echo "$3" >> password.txt
+echo "$4" >> gh-owner.txt
+echo "$5" >> gh-repository.txt
+echo "$6" >> gh-access-token.txt
+echo "$7" >> runner-tag.txt
 
-cat temp.txt
+
+cat docker-repo.txt
+
+cat password.txt | docker login -u $2 --password-stdin
+#docker build -t mmullin12/pong:latest -f ./ClientServer/app.DockerFile ./ClientServer
+#docker push mmullin12/pong:latest
--- image/app.Dockerfile
+++ image/app.Dockerfile
@@ -1,14 +1,7 @@
-# Container image that runs your code
-FROM alpine:3.10
+FROM ubuntu:20.x
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
-
-COPY /image /image
-
-RUN apk add --update docker openrc
-RUN addgroup root docker
-RUN rc-update add docker boot
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
