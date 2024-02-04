512b820ea59f53a3fe9af2b93af1d60be119b5eb
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-04 02:14:59 -0500

    scripted commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,17 @@ echo "$6" >> runner-tag.txt
 
 
 
+
 wget https://github.com/docker/docker-credential-helpers/releases/download/v0.6.2/docker-credential-secretservice-v0.6.2-amd64.tar.gz
 tar -xf docker-credential-secretservice-v0.6.2-amd64.tar.gz
+
 chmod +x docker-credential-secretservice
-#mv docker-credential-secretservice /bin/
-pwd
-ls
+#mkdir credential-location
+#mv docker-credential-secretservice credential-location
+
+mv docker-credential-secretservice /usr/local/bin
+
+#export PATH=$PATH:
 #sed -i '0,/{/s/{/{\n\t"credsStore": "secretservice",/' ./.docker/config.json
 
 cat password.txt | docker login -u $2 --password-stdin
