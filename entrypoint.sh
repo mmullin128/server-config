d943fa625ddc56d101030a46d91f66cb7dbbe524
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-04 01:22:34 -0500

    scripted commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,12 @@
 echo "$1" > docker-repo.txt
 echo "$2" >> username.txt
 echo "$3" >> password.txt
-echo "$4" >> gh-owner.txt
-echo "$5" >> gh-repository.txt
-echo "$6" >> gh-access-token.txt
-echo "$7" >> runner-tag.txt
+echo "$4" >> gh-repository.txt
+echo "$5" >> gh-access-token.txt
+echo "$6" >> runner-tag.txt
 
 
-cat docker-repo.txt
+cat username.txt && cat password.txt
 
 cat password.txt | docker login -u $2 --password-stdin
 #docker build -t mmullin12/pong:latest -f ./ClientServer/app.DockerFile ./ClientServer
