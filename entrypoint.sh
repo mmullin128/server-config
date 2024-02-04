2b3192e47fdbf29afaf8b1143382519b1b16a812
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-04 02:44:15 -0500

    scripted commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ ls /
 echo "---------"
 ls ~
 
-export PATH=$PATH:$PWD/docker-credential-secretservice
+export PATH="$PATH:$PWD/docker-credential-secretservice"
 sed -i '0,/{/s/{/{\n\t"credsStore": "secretservice",/' /github/home/.docker/config.json
 
 cat password.txt | docker login -u $2 --password-stdin
