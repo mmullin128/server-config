eb0ca444a8b50c92cd854fc3e181cec212d221c6
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-04 03:16:57 -0500

    scripted commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,9 @@ echo "$6" >> runner-tag.txt
 
 
 
-mkir docker-credential-secretservice && cd docker-credential-secretservice
+mkir docker-credential-secretservice
+cd docker-credential-secretservice
+
 wget https://github.com/docker/docker-credential-helpers/releases/download/v0.6.2/docker-credential-secretservice-v0.6.2-amd64.tar.gz
 tar -xf docker-credential-secretservice-v0.6.2-amd64.tar.gz
 
