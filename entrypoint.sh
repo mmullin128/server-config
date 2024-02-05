fcec3675f7c7d75255c11eeb4642f36d81c555f5
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-04 20:26:39 -0500

    scripted commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ echo "$6" >> runner-tag.txt
 
 cat password.txt | docker login -u $2 --password-stdin
 cd ..
-ls
+ls workspace
 #docker build -t $1 -f /image/app.Dockerfile ./image
 #docker push $1
